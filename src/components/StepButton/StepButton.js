9b9f194aadb582e4a63d74a10e7c97bdf9caf76f
davide <davide.lista@mail.polimi.it>
2021-08-11 11:39:47 +0200

    add onclick step button

--- src/components/StepButton/StepButton.js
+++ src/components/StepButton/StepButton.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './StepButton.module.css'
 
 const StepButton = (props) => {
+    const [active, setActive] = useState(false);
     const {children} = props
     return (
         <>
-            <div className={styles.container}>
+            <div className={active ? styles.active : styles.container} onClick={() => setActive(!active)}>
                 {children}
             </div>
 

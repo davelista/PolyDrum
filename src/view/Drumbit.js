6c2bf60eda7fce4a55158e46cad519d3e1f18ee9
davelista <davide.lista@mail.polimi.it>
2021-08-17 14:58:30 +0200

    commit for albe

--- src/view/Drumbit.js
+++ src/view/Drumbit.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styles from "./Drumbit.module.css";
-import {CheckBox, ControlSlider, Dropdown, NumberControl, SampleLine, StepButton} from "../components";
+import {CheckBox, ControlSlider, Dropdown, NumberControl, SampleLine, StepButton, Button} from "../components";
+import {AppContext} from "../context/AppContext";
+
 
 function Drumbit(props) {
+    const appData = useContext(AppContext);
     return (
         <div className={styles.container}>
+
             <CheckBox label={"Pippo"}/>
 
             <Dropdown/>
             <SampleLine/>
-            <ControlSlider/>
+            <ControlSlider title={"Volume"} defaultValue={appData.volume.value} onChangeValue={appData.volume.setValue}/>
             <StepButton/>
-            <NumberControl/>
+            <Button onClick={() => appData.play.setPlay(!appData.play.isPlay)} />
+            {console.log("play è: ", appData.play.isPlay)}
+            {console.log("ho cambiato il TEMPO in: ", appData.tempo.bpm)}
+            {console.log("ho cambiato il VOLUME in: ", appData.volume.value)}
+            {/*NON ELIMINARE NUMBERCONTROL*/}
+            <NumberControl tempo={appData.tempo} play={appData.play}/>
         </div>
     );
 }

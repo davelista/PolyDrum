d3a7c0fe18876dfb08d6454fa00eb0838a2b9246
Alberto <alberto.dimaria@mail.polimi.it>
2021-09-01 19:41:12 +0200

    Both popups changes

--- src/components/Popup/DetailsPopup.js
+++ src/components/Popup/DetailsPopup.js
@@ -6,26 +6,27 @@ const DetailsPopup = () => {
             Time signature is defined in the following way:
             <ul>
                 <li><b>m/n</b> --> m beats, each an n-th of the whole note (first)</li>
-                <li><b>i/j</b> --> m beats, each an n-th of the whole note (second)</li>
+                <li><b>i/j</b> --> i beats, each an j-th of the whole note (second)</li>
             </ul>
 
             <ul>
-                <li>The first rhythm fits a measure of m beats, each lasting 1/m of a whole note</li>
+                <li>The first rhythm fits a measure of m beats, each lasting 1/n of a whole note</li>
                 <li>The second rhythm fits a measure of i beats, each lasting 1/j of a whole note</li>
             </ul>
-            A complex rhythm is indicated as <b>m/n : i/j</b><br/><br/>
-            Single signature that accommodates both sub-signatures of a polymetric structure:
+            A complex rhythm is indicated as <b>m/n : i/j</b><br/>
+            To find a single signature that accommodates both sub-signatures of a polymetric structure:
             <ul>
                 <li>N<sub>i</sub> / D<sub>i</sub> --> i rhythm</li>
                 <li>N<sub>1</sub> / D<sub>1</sub> : N<sub>2</sub> / D<sub>2</sub> --> N / D</li><br/>
-                Where:
+                Where, in the general case:
                 <ul>
                     <li>D = lcm (D<sub>1</sub>, D<sub>2</sub>)</li>
                     <li>N = lcm (N<sub>1</sub>K<sub>1</sub>, N<sub>2</sub>K<sub>2</sub>)</li>
                     <li> K<sub>i</sub> = D / D<sub>i</sub></li>
                 </ul>
             </ul>
-            If D > D<sub>1</sub> and D > D<sub>2</sub> the info of the pulse is lost (ex. 4/4:7/7 --> 28/28).
+            <b>In this implementation n = j, meaning that the PolyDrum is polymetric.
+            D is a fixed value equal to 4.</b>
         </div>
     </>
 }
--- src/components/Popup/InstructionPopup.js
+++ src/components/Popup/InstructionPopup.js
@@ -11,7 +11,7 @@ const InstructionPopup = (props) => {
             </div>
             <div style={{display: "flex", height: "3rem", alignItems:"center"}}>
                 <Button> All <BiSelectMultiple size={20}/></Button>
-                <div style={{marginLeft: "1rem"}}>button to select all rhythms</div>
+                <div style={{marginLeft: "1rem"}}>button to display and play all rhythms together</div>
             </div>
             <div style={{display: "flex", height: "3rem", alignItems:"center"}}>
                 <Button><FaPlus/></Button>
@@ -19,17 +19,17 @@ const InstructionPopup = (props) => {
             </div>
             <div style={{display: "flex", height: "3rem", alignItems:"center"}}>
                 <Button><FaMinus/></Button>
-                <div style={{marginLeft: "1rem"}}>button to delete a rhythm</div>
+                <div style={{marginLeft: "1rem"}}>button to delete the last rhythm</div>
             </div>
             <div style={{display: "flex", height: "3rem", alignItems:"center"}}>
                 <Button><BiHash/></Button>
                 <div style={{marginLeft: "1rem"}}>button to select a specific rhythm</div>
             </div>
             <br/>
-            On the right the drum is divided:
+            On the right the PolyDrum is so divided:
             <ul>
-                <li>On top: <b>display to see the final rhythm</b></li>
-                <li>On bottom: <b>pad to characterize your rhythms</b></li>
+                <li>On top: <b>display, to see the final rhythm</b></li>
+                <li>On bottom: <b>pad, to custom your rhythms</b></li>
             </ul>
         </div>
     </>);

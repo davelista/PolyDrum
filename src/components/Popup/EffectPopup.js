786b81a26295f2167c9f5f3ede1bea2337d1bafb
Alberto <alberto.dimaria@mail.polimi.it>
2022-02-10 17:38:24 +0100

    New branch changes

--- src/components/Popup/EffectPopup.js
+++ src/components/Popup/EffectPopup.js
@@ -7,31 +7,31 @@ return <>
     <div>
         Here is the list of available effect:
         <ul>
-            <li><b>autoFilter</b>:</li>
+            <li><b>autoFilter</b>: versatile filter effect with several unique features</li>
         </ul>
         <ul>
-            <li><b>autoPanner</b>:</li>
+            <li><b>autoPanner</b>: panner with an LFO connected to the pan amount</li>
         </ul>
         <ul>
-            <li><b>autoWah</b>:</li>
+            <li><b>autoWah</b>: type of wah-wah effects pedal</li>
         </ul>
         <ul>
-            <li><b>bitCrusher</b>:</li>
+            <li><b>bitCrusher</b>: distortion by reducing the resolution or bandwidth of digital audio data</li>
         </ul>
         <ul>
-            <li><b>distortion</b>:</li>
+            <li><b>distortion</b>: alteration of the original shape of the signal, based on hard-clipping</li>
         </ul>
         <ul>
-            <li><b>feedbackDelay</b>:</li>
+            <li><b>feedbackDelay</b>: time between the signal and its feedback</li>
         </ul>
         <ul>
-            <li><b>freeVerb</b>:</li>
+            <li><b>freeVerb</b>: stereo reverb</li>
         </ul>
         <ul>
-            <li><b>panVol</b>:</li>
+            <li><b>panVol</b>:controls the pan of the sound and its volume</li>
         </ul>
         <ul>
-            <li><b>tremolo</b>:</li>
+            <li><b>tremolo</b>: modulation that rhythmically changes the volume</li>
         </ul>
 
         How to use these effects?
